e18f940a2099561eea10ed063e4d441a3e68b065
Kelvin <kelvin.ishigo@gmail.com>
2025-05-22 08:19:59 -0700

    Use send_text_email from hilo_software_utilities

--- scripts/washer_dryer_notifier.py
+++ scripts/washer_dryer_notifier.py
@@ -20,6 +20,8 @@ import sys
 import inspect
 import requests
 from logging.handlers import TimedRotatingFileHandler
+from hilo_software_utilities.send_mail import send_text_email
+
 
 LOG_FILE = "washer_dryer_notifier.log"
 CONFIG_FILE = "washer_dryer_notifier.config"
@@ -263,50 +265,50 @@ def get_power(plug: SmartDevice) -> float:
     return plug.emeter_realtime.power
 
 
-def send(from_addr: str, to_addr: str, app_key: str, msg: EmailMessage) -> None:
-    '''
-    Constructs and sends email of log via SMTP for gmail.
-    Must have an app_key
-    Interested in different email, rewrite this.
+# def send(from_addr: str, to_addr: str, app_key: str, msg: EmailMessage) -> None:
+#     '''
+#     Constructs and sends email of log via SMTP for gmail.
+#     Must have an app_key
+#     Interested in different email, rewrite this.
 
-    Args:
-        from_addr (_type_): _description_
-        to_addr (_type_): _description_
-        app_key (_type_): _description_
-        msg (_type_): _description_
-    '''
-    try:
-        logger.info(f'[EMAIL] send')
-        smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
-        smtpobj.ehlo()
-        smtpobj.starttls()
-        smtpobj.ehlo()
-        smtpobj.login(from_addr, app_key)
-        smtpobj.sendmail(from_addr, to_addr, msg.as_string())
-        smtpobj.close()
-        logger.info(f'[EMAIL] sent')
-    except smtplib.SMTPException as e:
-        logger.error(f'MAIL SMTP ERROR: Unable to send mail: {str(e)}')
-    except Exception as e:
-        logger.error(f'MAIL General ERROR: Unexpected Exception in send: Unable to send mail: {str(e)}')
+#     Args:
+#         from_addr (_type_): _description_
+#         to_addr (_type_): _description_
+#         app_key (_type_): _description_
+#         msg (_type_): _description_
+#     '''
+#     try:
+#         logger.info(f'[EMAIL] send')
+#         smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+#         smtpobj.ehlo()
+#         smtpobj.starttls()
+#         smtpobj.ehlo()
+#         smtpobj.login(from_addr, app_key)
+#         smtpobj.sendmail(from_addr, to_addr, msg.as_string())
+#         smtpobj.close()
+#         logger.info(f'[EMAIL] sent')
+#     except smtplib.SMTPException as e:
+#         logger.error(f'MAIL SMTP ERROR: Unable to send mail: {str(e)}')
+#     except Exception as e:
+#         logger.error(f'MAIL General ERROR: Unexpected Exception in send: Unable to send mail: {str(e)}')
 
 
-def send_my_mail(email: str, app_key: str, appliance: Appliance) -> None:
-    if email == None or app_key == None:
-        print('Email args missing not sending')
-    else:
-        logger.info(f'[EMAIL] send_my_mail')
-        # Create a text/plain message
-        msg = EmailMessage()
+# def send_my_mail(email: str, app_key: str, appliance: Appliance) -> None:
+#     if email == None or app_key == None:
+#         print('Email args missing not sending')
+#     else:
+#         logger.info(f'[EMAIL] send_my_mail')
+#         # Create a text/plain message
+#         msg = EmailMessage()
 
-        msg.set_content(f"{appliance.get_appliance_name} is FINISHED")
+#         msg.set_content(f"{appliance.get_appliance_name} is FINISHED")
 
-        # me == the sender's email address
-        # you == the recipient's email address
-        msg['Subject'] = f'washer dryer notifier'
-        msg['From'] = f'{email}'
-        msg['To'] = f'{email}'
-        send(email, email, app_key, msg)
+#         # me == the sender's email address
+#         # you == the recipient's email address
+#         msg['Subject'] = f'washer dryer notifier'
+#         msg['From'] = f'{email}'
+#         msg['To'] = f'{email}'
+#         # send_text_email(email=email, app_key=app_key, subject='washer dryer notifier', content=f"{appliance.get_appliance_name} is FINISHED")
 
 
 async def notify_finished(appliance: Appliance, notifier_script: str = None, email_context = None) -> None:
@@ -316,9 +318,10 @@ async def notify_finished(appliance: Appliance, notifier_script: str = None, ema
     if pbb == None:
         logger.error(f"notify_finished(), no pushbullet specified, will not notify channel")
     else:
-        pbb.send_notification(f"{appliance.get_appliance_name()}", f"FINISHED")
+        msg_string = f"{appliance.get_appliance_name()}", f"FINISHED"
+        pbb.send_notification(msg_string)
         if email_context != None:
-            send_my_mail(email_context.email, email_context.app_key, appliance)
+            send_text_email(email=email_context.email, app_key=email_context.app_key, subject=f'washer dryer notifier', content=msg_string)
     if notifier_script is not None:
         process = await asyncio.create_subprocess_exec("python3", notifier_script)
         await process.wait()
